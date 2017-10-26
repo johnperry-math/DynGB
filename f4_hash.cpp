5069f8a9b8380d1b049cab51d1e68fb211d1a08a
John Perry <john.perry@usm.edu>
2017-10-26 16:35:01 -0500

    implemented hash table in f4_reduction; encountered a problem in sugar_strategy where gcc does not seem to understand the principle of inheritance

---
+++ f4_hash.cpp
@@ -0,0 +1,13 @@
+#include "f4_hash.hpp"
+
+ostream & operator << (ostream & os, const F4_Hash & ht) {
+  for (size_t i = 0; i < ht.MAXIMUM; ++i) {
+    if (ht.table[i].size() != 0) {
+      os << i << ": ";
+      for (auto iter = ht.table[i].begin(); iter != ht.table[i].end(); ++iter)
+        os << '(' << *(iter->first) << ',' << iter->second << ") ";
+      os << endl;
+    }
+  }
+  return os;
+}
